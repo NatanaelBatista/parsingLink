493bd674eaaa5792a514ba0679064171650710e1
root <root@kali>
2021-03-20 11:49:19 -0300

    Crição do script

---
+++ parsingLink.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+
+figlet "ParsingLink"
+
+if [ "$1" == "" ]
+then
+echo "Uso: $0 www.site.com"
+else
+if [ -e index.html ]
+then
+rm index.html
+fi
+wget "$1"
+echo "Links encontrados:"
+grep href index.html | cut -d ":" -f2 | cut -d "/" -f3 | grep "\." | grep -v "(" | grep -v "href" | grep -v "," | cut -d " " -f1 | cut -d "?" -f1 | sed 's/"//' | sed 's/>//' | sort -u > list
+
+for url in $(cat list)
+do
+host $url | grep "has address"
+done
+fi
