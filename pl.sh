548098efefb42ad9ad603082cf15c625e3a37282
root <root@kali>
2021-03-20 13:53:35 -0300

    Atualization

--- pl.sh
+++ pl.sh
@@ -4,14 +4,14 @@ figlet "ParsingLink"
 
 if [ "$1" == "" ]
 then
-echo "Uso: $0 www.site.com"
+echo "Use: $0 www.example.com"
 else
 if [ -e index.html ]
 then
 rm index.html
 fi
 wget "$1"
-echo "Links encontrados:"
+echo "Links found:"
 grep href index.html | cut -d ":" -f2 | cut -d "/" -f3 | grep "\." | grep -v "(" | grep -v "href" | grep -v "," | cut -d " " -f1 | cut -d "?" -f1 | sed 's/"//' | sed 's/>//' | sort -u > list
 
 for url in $(cat list)
